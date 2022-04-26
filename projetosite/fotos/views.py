488d1e5a1c644eb9248354b14e34a2967ab1814b
kreativermario <80268327+kreativermario@users.noreply.github.com>
2022-04-26 18:16:59 +0100

    django

--- projetosite/fotos/views.py
+++ projetosite/fotos/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
-
+from .models import Categoria, Foto
 # Create your views here.
 
 def galeria(request):
-    return render(request, 'fotos/galeria.html')
+    categorias = Categoria.objects.all()
+    fotos = Foto.objects.all()
+    context = {
+        'categorias': categorias,
+        'fotos' : fotos
+    }
+
+    return render(request, 'fotos/galeria.html', context)
 
 
 def verFoto(request, pk):
-    return render(request, 'fotos/foto.html')
+    foto = Foto.objects.get(id=pk)
+    context = {
+        'foto': foto
+    }
+    return render(request, 'fotos/foto.html', context)
 
 
 def criarFoto(request):
-    return render(request, 'fotos/criar.html')
+    categorias = Categoria.objects.all()
+    context = {
+        'categorias': categorias,
+    }
+    return render(request, 'fotos/criar.html', context)
